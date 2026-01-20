49a722d5ebe72225cd2f26805e51580c73385b7b
Beloslava <beloslavamincheva1@gmail.com>
2026-01-20 13:51:44 +0200

    Add project files.

---
+++ MelodyHub 2.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MelodyHub_2._0.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelodyHub_2._0.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Този ред генерира таблицата с продукти
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MelodyHub 2.0/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MelodyHub_2._0.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Името е задължително")]
+        [Display(Name = "Инструмент")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.01, 100000)]
+        [Column(TypeName = "decimal(18,2)")]
+        [Display(Name = "Цена (лв.)")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Категория")]
+        public string Category { get; set; } = "Музикален инструмент";
+
+        [Display(Name = "URL на снимка")]
+        public string ImageUrl { get; set; } = "https://images.unsplash.com/photo-1511379938547-c1f69419868d?w=500";
+
+        [Display(Name = "Описание")]
+        public string Description { get; set; } = "Висок клас музикално оборудване.";
+    }
+}
